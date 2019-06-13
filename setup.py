e38dd967f8db68967f6ae0131e5e6a19557966fb
Jerome Kieffer <jerome.kieffer@esrf.fr>
2019-06-13 14:28:50 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dynamix',
+      version='0.0.1',
+      description='XPCS code',
+      author='Jerome Kieffer',
+      author_email='jerome.kieffer@esrf.fr',
+      url='https://github.com/kif/dynamix',
+      packages=['dynamix'],
+     )
