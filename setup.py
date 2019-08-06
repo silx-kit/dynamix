58faa4408dea7260917877fb895a81b6aed6f9e0
Pierre Paleo <pierre.paleo@esrf.fr>
2019-08-06 16:23:21 +0200

    Bootstrap project structure

--- setup.py
+++ setup.py
@@ -1,12 +1,67 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import os
 
-setup(name='dynamix',
-      version='0.0.1',
-      description='XPCS code',
-      author='Jerome Kieffer',
-      author_email='jerome.kieffer@esrf.fr',
-      url='https://github.com/kif/dynamix',
-      packages=['dynamix'],
-     )
+def get_version():
+    with open("dynamix/__init__.py", "r") as fid:
+        lines = fid.readlines()
+    version = None
+    for line in lines:
+        if "version" in line:
+            version = line.rstrip().split("=")[-1].lstrip()
+    if version is None:
+        raise RuntimeError("Could not find version from __init__.py")
+    version = version.strip("'").strip('"')
+    return version
+
+
+def setup_package():
+
+    version = get_version()
+
+    packages_folders = [
+        "correlator",
+        "resources",
+    ]
+    packages = ["dynamix"]
+    package_dir = {"dynamix": "dynamix"}
+    tests_folders = []
+    for f in packages_folders:
+        modulename = str("dynamix.%s" % f)
+        packages.append(modulename)
+        package_dir[modulename] = os.path.join("dynamix", f)
+        module_test_dirname = os.path.join(package_dir[modulename], "tests")
+        if os.path.isdir(module_test_dirname):
+            modulename_test = str("%s.tests" % modulename)
+            packages.append(modulename_test)
+            package_dir[modulename_test] = module_test_dirname
+    setup(
+        name='dynamix',
+        author='Pierre Paleo, Jerome Kieffer',
+        maintainer='Pierre Paleo, Jerome Kieffer',
+        version=version,
+        author_email = "pierre.paleo@esrf.fr",
+        maintainer_email = "pierre.paleo@esrf.fr",
+        url='https://github.com/silx-kit/dynamix',
+
+        packages=packages,
+        package_dir = package_dir,
+        package_data = {
+            'dynamix.resources': [
+                'opencl/*.cl',
+            ]
+        },
+        install_requires = [
+          'numpy',
+        ],
+        long_description = """
+        dynamix - software for X-ray photon correlation spectroscopy
+        """,
+
+        zip_safe=True
+    )
+
+
+if __name__ == "__main__":
+    setup_package()
