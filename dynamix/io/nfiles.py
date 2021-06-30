8f80e76844dd1c86ad323a685be0860beac6020e
Jérôme Kieffer <jerome.kieffer@terre-adelie.org>
2021-06-30 15:19:15 +0200

    clean-up

--- dynamix/io/nfiles.py
+++ dynamix/io/nfiles.py
@@ -4,7 +4,6 @@ def filename(pref, suf, firstf, lastf):
     fname = []
     for i in range(len(numb)):
         if numb[i] < 10000:
-            # fname[i]=pref+string.zfill(str(numb[i]),4)+suf
             fname.append(pref + str(numb[i]).zfill(4) + suf)
         else:
             fname.append(pref + str(numb[i]) + suf)
