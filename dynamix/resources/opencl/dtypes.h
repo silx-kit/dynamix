6d8503df75769132cf204fbbc5abd6798d2ec2c9
Pierre Paleo <pierre.paleo@esrf.fr>
2023-05-30 15:55:36 +0200

    Move some OpenCL code in various files

---
+++ dynamix/resources/opencl/dtypes.h
@@ -0,0 +1,27 @@
+#ifndef DTYPES_H
+#define DTYPES_H
+
+#ifndef DTYPE
+  #define DTYPE uchar
+#endif
+
+#ifndef OFFSET_DTYPE
+  // must be extended to unsigned long if total_nnz > 4294967295
+  #define OFFSET_DTYPE uint
+#endif
+
+#ifndef SHARED_ARRAYS_SIZE
+  #define SHARED_ARRAYS_SIZE 4096
+#endif
+
+#ifndef QMASK_DTYPE
+  // must be extended to int if number of q-bins > 127
+  #define QMASK_DTYPE char
+#endif
+
+#ifndef RES_DTYPE
+  // must be extended to unsigned long for large nnz_per_frame and/or events counts
+  #define RES_DTYPE uint
+#endif
+
+#endif 
